dbd5be1a3b892ba5e5443647c23b89c41275726a
Han4578 <99018603+Han4578@users.noreply.github.com>
2023-05-21 18:45:05 +0800

    added new logo and redirect pages

--- require/menu.js
+++ require/menu.js
@@ -1,14 +1,24 @@
 let body = document.querySelector('body')
 let menu = document.querySelector('#menu')
+let user = document.querySelector('#user')
+let userMenu = document.querySelector('#user-menu')
 let menuList = document.querySelector('#menu-list')
 let darken = document.querySelector('.darken')
 let home = document.querySelector('[data-home]')
 let filter = document.querySelector('[data-filter]')
+let logo = document.querySelector('[data-logo]')
+let login = document.querySelector('[data-login]') ?? document.createElement('div')
+let profile = document.querySelector('[data-profile]') ?? document.createElement('div')
 
 menu.addEventListener('click', openMenu)
 darken.addEventListener('click', closeMenu)
 home.addEventListener('click', redirectHome)
 filter.addEventListener('click', redirectFilter)
+login.addEventListener('click', redirectLogin)
+profile.addEventListener('click', redirectProfile)
+logo.addEventListener('click', redirectHome)
+window.addEventListener('click', closeUser)
+user.addEventListener('click', openUser)
 
 function openMenu() {
     menuList.classList.add('open')
@@ -31,5 +41,22 @@ function redirectHome() {
 
 function redirectFilter() {
     window.location = '../main_pages/filter.php';
+}
+
+function redirectLogin() {
+    window.location = '../user register/user_register.php';
+}
+
+function redirectProfile() {
+    window.location = '../main_pages/profile.php';
+}
+
+function openUser(e) {
+    userMenu.classList.add('open')
+    e.stopPropagation()
+}
+
+function closeUser() {
+    userMenu.classList.remove('open')
 }
 
--- require/register_menu.php
+++ require/register_menu.php
@@ -1,16 +1,28 @@
+<?php
+    require "../require/connect.php";
+    session_start();
+?>
+
 <link rel="stylesheet" href="../style.css">
 <script src="../functions.js"></script>
 
-<div class="atas">
-    <div class="ikon close" id="menu">
+<div class="top">
+    <div class="icon close" id="menu">
         <img src="../images/Hamburger_icon.png" alt="hamburger-icon">
     </div>
-    <h1>FURNITREE</h1>
-    <div class="ikon">
-        <img src="../images/user_icon.png" id="user" alt="user-icon">
+    <img src="../images/furnitree logo full.png" alt="furnitree logo" data-logo>
+    <div class="icon" id="user">
+        <img src="../images/user_icon.png" alt="user-icon">
     </div>
 </div>
-<div class="darken close"></div>
+<div id="user-menu">
+    <?php         
+        echo (key_exists('isLogged in', $_SESSION) and $_SESSION['isLoggedIn'] == 1)? "<div class='user-button' data-profile>Profil</div>": "<div class='user-button' data-login>Log masuk</div>";
+    ?>
+    <div class="user-button">Profil</div>
+    <div class="user-button">Profil</div>
+</div>
+<div class="darken"></div>
 <div id="menu-list">
     <h1 class="menu-logo">Menu</h1>
     <div class="menu-button" data-home>home</div>
@@ -20,8 +32,8 @@
 
 
 <script>
-    <?php 
-        require "menu.js";
+    <?php
+    require "menu.js";
     ?>
 
     function openMenu() {
@@ -29,6 +41,7 @@
         darken.classList.add('open')
         body.style.overflow = 'hidden'
     }
+
     function closeMenu(e) {
         menuList.classList.remove('open')
         darken.classList.remove('open')
