31573d58aa6049a12e658933c7508a02e9f4a05f
Han4578 <99018603+Han4578@users.noreply.github.com>
2023-06-01 19:30:50 +0800

    created base model of product page

---
+++ main_pages/product.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require "../require/register_menu.php";
+    ?>
+    <br>
+    <div class="main split">
+        <div class="product-info">
+            <div class="space-between">
+                <div>
+                    <div class="name">Paseo Office Chair</div>
+                    <div class="company">Dari Latitude Walk</div>
+                </div>
+                <div class="price">RM400</div>
+            </div>
+            <div>
+                Diskripsi: <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc facilisis pretium tellus, quis pulvinar diam varius sit amet. In sit amet quam pretium, facilisis ante id, blandit justo. Donec a gravida diam, eget bibendum enim. Pellentesque vulputate commodo massa in tempor. In ut ligula maximus, efficitur mi sagittis, tempus eros.
+            </div>
+            <div>
+                Related: <br>
+                <div class="related-list">
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                    <div class="related-item">a</div>
+                </div>
+            </div>
+        </div>
+        <div class="product-container">
+            <div class="product-image">
+                <img  src="../images/placeholder.jpg" alt="">
+            </div>
+            <div class="color-list">
+                <div class="color" style="background-color: red;"></div>
+                <div class="color" style="background-color: green;"></div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
